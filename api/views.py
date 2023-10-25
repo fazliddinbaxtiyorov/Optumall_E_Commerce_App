49f286fdb3e474f6f70ce505af3084fd2ce0eb9e
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-10-25 23:54:08 +0500

    Added: Added class to view all products

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAdminUser
+
+from .models import Users, Profile, Products, Cart
+from .serializers import UserSerializer, ProfileSerializer, ProductsSerializer, CartSerializer
+
 
 # Create your views here.
+
+
+class ShowAllProducts(ListAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
