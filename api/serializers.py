e00e9aa5dd94d84e1fc7deaae33e2edf25f3aca2
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-10-25 23:46:26 +0500

    Added: CartSerializer to serialize Cart model

--- api/serializers.py
+++ api/serializers.py
@@ -23,3 +23,9 @@ class ProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Products
         fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
