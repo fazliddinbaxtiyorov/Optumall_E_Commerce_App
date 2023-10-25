8f9ee1f0fe5dc244c2cf46e3a0bbb3c63071daaa
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-10-25 23:30:42 +0500

    Created: Created Users table

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=60)
+    email = models.EmailField(max_length=90)
+    password = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.first_name
