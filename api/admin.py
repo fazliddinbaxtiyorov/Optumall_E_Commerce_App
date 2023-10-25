d999cb30dd07450cb1ae7734812fb82af5eff8a6
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-10-25 23:38:54 +0500

    Added: Registered our models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Users, Products, Profile, Cart
 
 # Register your models here.
+admin.site.register(Users)
+admin.site.register(Products)
+admin.site.register(Profile)
+admin.site.register(Cart)
