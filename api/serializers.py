1361b14b8b544c7d535e4a955fd0cecc3a4bd07f
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-10-25 23:42:48 +0500

    Added: UserSerializer to serialize our Users table

--- api/serializers.py
+++ api/serializers.py
@@ -1,1 +1,13 @@
+from rest_framework import serializers
+from django import forms
 
+from .models import Users, Profile, Products, Cart
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = '__all__'
+        widgets = {
+            "password": forms.PasswordInput()
+        }
