da8e71d11e9fb478e78a59273035ed9d03cf1d60
Fazliddin Baxtiyorov <137509986+fazliddinbaxtiyorov@users.noreply.github.com>
2023-10-26 22:00:38 +0500

    Adding: Some Urls

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from .views import ShowAllProducts, CreateNewProduct, UpdateProductById, DeleteProductById
 
+urlpatterns = [
+    path('api/products', ShowAllProducts.as_view()),
+    path('api/new', CreateNewProduct.as_view()),
+    path('api/<int:id>/update', UpdateProductById.as_view()),
+    path('api/delete/<int:id>/cart', DeleteProductById.as_view()),
+]
