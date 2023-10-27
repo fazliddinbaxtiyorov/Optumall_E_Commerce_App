e9442b49f9230d367ab187a8808c60ff36cf6d7f
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-10-27 20:25:58 +0500

    Added: Added urls

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,14 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('products', views.ShowAllProducts.as_view(), name='products'),
+    path('new', views.CreateNewProduct.as_view(), name='new'),
+    path('<int:id>/update', views.UpdateProductById.as_view(), name='update'),
+    path('category/<str:category>', views.Category.as_view(), name='search-with-category'),
+    path('product/<int:id>/delete', views.DeleteProductById.as_view(), name='delete'),
+    path('search?product=<str:title>', views.ProductList.as_view(), name='search'),
+]
 
