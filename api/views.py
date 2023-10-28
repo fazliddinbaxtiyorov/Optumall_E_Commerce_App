40c5ba18068e58339dccaf0c7b6741395edaa26b
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-10-28 20:39:33 +0500

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView
+from rest_framework import filters
+from rest_framework.response import Response
 
 from .models import Users, Profile, Products, Cart
 from .serializers import UserSerializer, ProfileSerializer, ProductsSerializer, CartSerializer
