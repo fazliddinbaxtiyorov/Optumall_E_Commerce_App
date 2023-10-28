77ba8d59f0fd063b9dcd45b83f8fd16638ff0e83
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-10-28 20:41:33 +0500

    Update settings.py

--- Optumall/settings.py
+++ Optumall/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+REST_FRAMEWORK = {
+    'SEARCH_PARAM': 'product'
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
