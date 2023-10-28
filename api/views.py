9b619db9cf54df832e449b432167f656b4eec146
Fazliddin Baxtiyorov <137509986+fazliddinbaxtiyorov@users.noreply.github.com>
2023-10-28 14:02:32 +0500

    Adding: All view and updated

--- api/views.py
+++ api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
-from rest_framework.permissions import IsAdminUser
-
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.authentication import SessionAuthentication
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.filters import SearchFilter
 from .models import Users, Profile, Products, Cart
 from .serializers import UserSerializer, ProfileSerializer, ProductsSerializer, CartSerializer
-
 
 # Create your views here.
 
@@ -26,8 +29,38 @@ class UpdateProductById(UpdateAPIView):
     lookup_field = 'id'
 
 
+class CategoryProductResultView(ListAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category']
+
+
+class SearchProductView(ListAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title']
+
+
 class DeleteProductById(DestroyAPIView):
-    queryset = Products
+    queryset = Products.objects.all()
     serializer_class = ProductsSerializer
     lookup_field = 'id'
     permission_classes = [IsAdminUser]
+
+
+class SeeUserInfo(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, username):
+        user = Users.objects.get(first_name=username)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
+class UpdateUserInfo(UpdateAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'username'
+    authentication_classes = [SessionAuthentication]
